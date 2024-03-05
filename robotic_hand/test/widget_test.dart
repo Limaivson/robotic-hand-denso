39687a595f076abbdaf56f453876243ecc72f2d2
Limaivson <ils3@cin.ufpe.br>
2024-03-05 13:33:46 -0300

    refactor

--- robotic_hand/test/widget_test.dart
+++ robotic_hand/test/widget_test.dart
@@ -7,13 +7,12 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:robotic_hand/main.dart';
+import 'package:robotic_hand/presenter/page/control.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const Control());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
